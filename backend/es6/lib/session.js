23fda6c8d99ed8858e41f5926a9d20181c261d08
septa97 <jancharles.adona@gmail.com>
2016-04-01 16:28:18 +0800

    Added session middleware

---
+++ backend/es6/lib/session.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const status        = require(__dirname + '/../lib/status');
+
+
+module.exports = (req, res, next) => {
+
+    if (typeof res === 'undefined') {
+        let role = req;
+
+        return (req, res, next) => {
+            let response;
+
+            // If there is no active session, and this middleware function is called,
+            // therefore, the current user is unauthorized
+            if (!req.session.user) {
+                response = status.UNAUTHORIZED;
+
+                return res.status(response.status).send(response.message);
+            }
+
+            if (role === req.session.user.role ||
+                    (req.session.user.role === 'Administrator' && role === 'Administrator'))
+            {
+                return next();
+            }
+
+            // If the role specified does not match, notify that the user is unauthorized
+            response = status.UNAUTHORIZED;
+
+            return res.status(response.status).send(response.message);
+        };
+    }
+};
