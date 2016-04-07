e8330baafc4ae8ed0912b3e9a282fb0d48c4c9d6
noreenlouisecb <noreenlouiseb15@gmail.com>
2016-04-07 22:56:40 +0800

    Modify register.controller.js

--- frontend/public/partials/register/register.controller.js
+++ frontend/public/partials/register/register.controller.js
@@ -11,9 +11,11 @@
     $scope.faculty_user_data = [];
 
     $scope.AddFacultyUser = function(){
-			RegisterService.AddFacultyUser($scope.newFacultyUser)
+      $scope.newFacultyUser.faculty_user_username;
+
+      RegisterService.AddFacultyUser($scope.newFacultyUser)
 				.then(function(data) {
-					$scope.faculty_user_data.push(data);  
+					$scope.faculty_user_data.push(data);
 				});
         //Materialize.toast('Faculty User added!', 8000, 'rounded');
         alert('Faculty User added!');
