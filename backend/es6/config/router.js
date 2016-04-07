af4e6d1bb45b83b5e392ba00b1a1b19d92fb2ef1
mariannecute <marianneluching@gmail.com>
2016-04-08 04:47:17 +0800

    Modify regiter.view.html

--- backend/es6/config/router.js
+++ backend/es6/config/router.js
@@ -12,7 +12,8 @@ module.exports = (router) => {
     router.get('/',                                                                     __.render_controller.index);
 
     router.post('/faculty_user/register/',                                              __.faculty_user.register);
-    router.post('/faculty_user/check_faculty_user_username/', 	                                __.faculty_user.check_faculty_user_username);
+    router.post('/faculty_user/check_faculty_user_username/',
+    router.post('/faculty_user/check_faculty_user_employee_id/', 	                                __.faculty_user.check_faculty_user_username);
     router.post('/faculty_user/post_volunteer/',                                        __.faculty_user.post_volunteer);
     router.get ('/faculty_user/volunteers/:user_id',                                    __.faculty_user.get_volunteers);
     router.put ('/faculty_user/volunteer/:user_id/:student_number',                     __.faculty_user.update_volunteer);
---
+++ frontend/public/partials/register/register.check_faculty_user_employee_id.js
@@ -0,0 +1,31 @@
+(function(){
+  'use strict';
+
+	angular
+		.module("app")
+		.controller("CheckEmpIDCtrl", CheckEmpIDCtrl);
+
+	CheckEmpIDCtrl.$inject = ["$scope", "$http"];
+
+	function CheckEmpIDCtrl($scope, $http){
+		angular.element(document.querySelector('#employee-input')).on('keyup',function(e){
+			var faculty_user_employee_id = document.querySelector('#employee-input').value;
+			if (faculty_user_employee_id===""){
+				document.querySelector('#uid-warning').innerText = "";
+				return;
+			}
+
+			$http.post(
+				"faculty_user/check_faculty_user_employee_id/",
+				 {faculty_user_employee_id: faculty_user_employee_id}
+			).then(function successCallback(response) {
+				if (response.data==true){
+					document.querySelector('#uid-warning').innerText = "Employee ID not available!";
+				} else {
+					document.querySelector('#uid-warning').innerText = "Employee ID available!";
+				}
+			}, function errorCallback(response) { });
+		});
+	}
+
+})();
