41c78f37b4b39419ae410fa8782e11d581f5a7ff
noreenlouisecb <noreenlouiseb15@gmail.com>
2016-04-08 00:23:06 +0800

    Modify register.view.html and register.check_faculty_user_username.js

--- frontend/public/partials/register/register.check_faculty_user_username.js
+++ frontend/public/partials/register/register.check_faculty_user_username.js
@@ -1,32 +1,31 @@
 (function(){
   'use strict';
 
-  angular
-    .module('app')
-    .controller('CheckUserCtrl', CheckUserCtrl);
+	angular
+		.module("app")
+		.controller("CheckUserCtrl", CheckUserCtrl);
 
-  CheckUserCtrl.$inject=["$scope", "$http"];
+	CheckUserCtrl.$inject = ["$scope", "$http"];
 
-  function CheckUserCtrl($scope, $http){
-	  angular.element(document.querySelector('#nameinput')).on('keyup',function(e){
-  		var faculty_user_username = document.querySelector('#nameinput').value;
-  		if (faculty_user_username === ""){
-  			document.querySelector('#uid-warning').innerText = "";
-  			return;
-  		}
+	function CheckUserCtrl($scope, $http){
+		angular.element(document.querySelector('#nameinput')).on('keyup',function(e){
+			var faculty_user_username = document.querySelector('#nameinput').value;
+			if (faculty_user_username===""){
+				document.querySelector('#uid-warning').innerText = "";
+				return;
+			}
 
-  		$http.post(
-        "faculty_user/check_faculty_user_username/",
-        {faculty_user_username: faculty_user_username}
-      ).then(function successCallback(response) {
-  			if (response.data==true){
-  				document.querySelector('#uid-warning').innerText = "Username not available!";
-  			} else {
-  				document.querySelector('#uid-warning').innerText = "Username available!";
-  			}
-		  }, function errorCallback(response) {
+			$http.post(
+				"faculty_user/check_faculty_user_username/",
+				 {faculty_user_username: faculty_user_username}
+			).then(function successCallback(response) {
+				if (response.data==true){
+					document.querySelector('#uid-warning').innerText = "Username not available!";
+				} else {
+					document.querySelector('#uid-warning').innerText = "Username available!";
+				}
+			}, function errorCallback(response) { });
 		});
-	 });
-  });
+	}
 
 })();
