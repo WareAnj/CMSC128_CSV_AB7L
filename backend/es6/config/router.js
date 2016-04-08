dfb3dd4a14f79d7daac04ef18b52005d323c6c11
septa97 <jancharles.adona@gmail.com>
2016-04-08 16:36:21 +0800

    Added session authentication

--- backend/es6/config/router.js
+++ backend/es6/config/router.js
@@ -9,24 +9,23 @@ module.exports = (router) => {
 
     router.del = router.delete;
 
-    router.get('/',                                                                     __.render_controller.index);
+    router.get('/',                                                                         __.render_controller.index);
 
-    router.post('/faculty_user/register/',                                              __.faculty_user.register);
-    router.post('/faculty_user/check_faculty_user_username/', __.faculty_user.check_faculty_user_username);
-    router.post('/faculty_user/check_faculty_user_employee_id/', 	                                __.faculty_user.check_faculty_user_employee_id);
-    router.post('/faculty_user/post_volunteer/',                                        __.faculty_user.post_volunteer);
-    router.get ('/faculty_user/volunteers',                                             __.faculty_user.get_volunteers);
-    router.put ('/faculty_user/volunteer/:user_id/:student_number',                     __.faculty_user.update_volunteer);
-    router.del ('/faculty_user/volunteer/',                                             __.faculty_user.delete_volunteer);
+    router.post('/faculty_user/register/',                                                  __.faculty_user.register);
+    router.post('/faculty_user/check_faculty_user_username/',                               __.faculty_user.check_faculty_user_username);
+    router.post('/faculty_user/check_faculty_user_employee_id/',                            __.faculty_user.check_faculty_user_employee_id);
+    router.post('/faculty_user/post_volunteer/',                        $('Faculty User'),  __.faculty_user.post_volunteer);
+    router.get ('/faculty_user/volunteers',                             $('Faculty User'),  __.faculty_user.get_volunteers);
+    router.put ('/faculty_user/volunteer/:user_id/:student_number',     $('Faculty User'),  __.faculty_user.update_volunteer);
+    router.del ('/faculty_user/volunteer/',                             $('Faculty User'),  __.faculty_user.delete_volunteer);
 
+    router.get ('/faculty_user/randomize',                              $('Faculty User'),  __.faculty_user.randomize);
 
-    router.get ('/faculty_user/randomize',                            __.faculty_user.randomize);
+    router.post('/authenticate/login/',                                                     __.authenticate.login);
+    router.get ('/authenticate/logout',                                 $,                  __.authenticate.logout);
 
-    router.post('/authenticate/login/',                                                 __.authenticate.login);
-    router.get ('/authenticate/logout',                             $,                  __.authenticate.logout);
-
-    router.get('/randomize_notuser',                                                    __.render_controller.randomize_notuser);
-    router.get('/randomize',                                      __.render_controller.randomize);
+    router.get('/randomize_notuser',                                                        __.render_controller.randomize_notuser);
+    router.get('/randomize',                                                                __.render_controller.randomize);
 
     router.all('*', (req, res, next) => {
         res.status(404)
