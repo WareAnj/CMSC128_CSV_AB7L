11bdc933d7422034f6d9851277d0ca9848a7ae79
septa97 <jancharles.adona@gmail.com>
2016-04-19 19:15:37 +0800

    Modified schema.sql

--- backend/es6/database/schema.sql
+++ backend/es6/database/schema.sql
@@ -33,10 +33,11 @@ CREATE TABLE faculty_user(
 -- Table 'course'
 DROP TABLE IF EXISTS course;
 CREATE TABLE course(
+    id INT NOT NULL AUTO_INCREMENT,
     code VARCHAR(16) NOT NULL,
     title VARCHAR(64) NOT NULL,
     description VARCHAR(256) NOT NULL,
-    PRIMARY KEY(code)
+    PRIMARY KEY(id)
 );
 
 
@@ -44,10 +45,12 @@ CREATE TABLE course(
 DROP TABLE IF EXISTS section;
 CREATE TABLE section(
     id INT NOT NULL AUTO_INCREMENT,
+    course_id INT NOT NULL,
     course_code VARCHAR(16) NOT NULL,
     name VARCHAR(8) NOT NULL,
     code VARCHAR(4),
     PRIMARY KEY(id),
+    FOREIGN KEY(course_id) REFERENCES course(id),
     FOREIGN KEY(course_code) REFERENCES course(code)
 );
 
@@ -69,23 +72,7 @@ CREATE TABLE student(
 );
 
 
-<<<<<<< HEAD
 -- Table 'student_section' separated the student table
-=======
--- Table 'sub_section'
-DROP TABLE IF EXISTS sub_section;
-CREATE TABLE sub_section(
-	section_id INT NOT NULL,
-	lecture_section_name VARCHAR(8) NOT NULL,
-	code VARCHAR(4) NOT NULL,
-	PRIMARY KEY(code),
-	FOREIGN KEY(section_id) REFERENCES section(id),
-	FOREIGN KEY(lecture_section_name) REFERENCES lecture_section(name)
-);
-
-
--- Table 'student_section'
->>>>>>> 2ef93e7a07f6afe04eb96cb24434432d6595d0aa
 DROP TABLE IF EXISTS student_section;
 CREATE TABLE student_section(
     student_id INT NOT NULL,
