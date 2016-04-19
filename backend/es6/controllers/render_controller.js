b55e6c80de5f116bc1ff590efbdd2580fbe00bba
joshzes <joshzerica@gmail.com>
2016-04-19 20:59:41 +0800

    Added new pages logs, home, and settings_randomize

--- backend/es6/controllers/render_controller.js
+++ backend/es6/controllers/render_controller.js
@@ -15,3 +15,15 @@ exports.edit = (req, res, next) => {
 exports.randomize_notuser = (req, res, next) =>{
 	res.render('randomize_notuser');
 };
+
+exports.logs = (req, res, next) =>{
+	res.render('logs');
+};
+
+exports.settings_randomize = (req, res, next) =>{
+	res.render('settings_randomize');
+};
+
+exports.home = (req, res, next) =>{
+	res.render('home');
+};
---
+++ frontend/public/js/default.js
@@ -0,0 +1,7 @@
+$(".button-sideNav").sideNav();
+
+$(document).ready(function(){
+  $('.collapsible').collapsible({
+    accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+  });
+});
