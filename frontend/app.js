ab1899700c5b434a88440d69d32c71fc5ca2c4d4
francisud <francis.deoferio@gmail.com>
2016-05-06 00:12:17 +0800

    Updated app.js, added admin routes

--- frontend/app.js
+++ frontend/app.js
@@ -19,6 +19,18 @@
                 'controller'  :   'AdminCtrl',
                 'templateUrl' :   'views/admin.html'
             })
+            .when('/admin/view-pending', {
+                'controller'  :   'AdminCtrl',
+                'templateUrl' :   'views/admin_approve.html'
+            })
+            .when('/admin/view-logs', {
+                'controller'  :   'AdminCtrl',
+                'templateUrl' :   'views/admin_viewlogs.html'
+            })
+            .when('/admin/view-approved', {
+                'controller'  :   'AdminCtrl',
+                'templateUrl' :   'views/admin_viewusers.html'
+            })
             .otherwise({
                 'redirectTo'  :   '/'
             });
