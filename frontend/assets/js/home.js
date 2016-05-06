8ec167c17b1148aaab00d8b6aae146f1e6ac5c13
joshzes <joshzerica@gmail.com>
2016-05-06 22:39:37 +0800

    Updated js files for reading user color settings

--- frontend/assets/js/home.js
+++ frontend/assets/js/home.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
 	$('ul.tabs').tabs();
 });
+
+$(document).ready(function(){
+    function get_file(){
+    	var cssf = $("#cssfile").html();
+        $("head").append("<link id='profile' type='text/css' rel='stylesheet' href='../assets/stylesheets/"+ cssf + "'>");
+    };
+
+	setTimeout(get_file, 300);
+});
