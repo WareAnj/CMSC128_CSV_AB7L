7df197454f76cc60fabc8e0b238c8dfb7d995f40
joshzes <joshzerica@gmail.com>
2016-05-06 23:47:18 +0800

    Dynamic loading of css files (not yet stored in db)

--- frontend/assets/js/home.js
+++ frontend/assets/js/home.js
@@ -10,3 +10,16 @@ $(document).ready(function(){
 
 	setTimeout(get_file, 300);
 });
+
+function displayProfile() {
+	var cssf = document.getElementById("profile-input").value + ".css";
+	
+	if(document.getElementById("profile")){
+		if(cssf != "default.css") $('#profile').replaceWith("<link id='profile' type='text/css' rel='stylesheet' href='../assets/stylesheets/"+ cssf + "'>");
+		else $('#profile').remove();
+	}
+
+	else{
+		$("head").append("<link id='profile' type='text/css' rel='stylesheet' href='../assets/stylesheets/"+ cssf + "'>");
+	}
+};
