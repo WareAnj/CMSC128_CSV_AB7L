92d6329b0b4b9420fad939330cb3fb3715b85968
WareAnj <ware.angelicap@gmail.com>
2016-05-08 12:44:17 +0800

    Updated highcharts

--- frontend/assets/js/admin.js
+++ frontend/assets/js/admin.js
@@ -5,76 +5,213 @@
 
     $('#container').highcharts({
         chart: {
-            type: 'column'
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
         },
         title: {
-            text: 'Efficiency Optimization by Branch'
-        },
-        xAxis: {
-            categories: [
-                'Seattle HQ',
-                'San Francisco',
-                'Tokyo'
-            ]
-        },
-        yAxis: [{
-            min: 0,
-            title: {
-                text: 'Employees'
-            }
-        }, {
-            title: {
-                text: 'Profit (millions)'
-            },
-            opposite: true
-        }],
-        legend: {
-            shadow: false
+            text: 'CMSC 128 AB Volunteer Distribution Per Lab Class'
         },
         tooltip: {
-            shared: true
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
         },
         plotOptions: {
-            column: {
-                grouping: false,
-                shadow: false,
-                borderWidth: 0
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %</a>',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                }
             }
         },
         series: [{
-            name: 'Employees',
-            color: 'rgba(165,170,217,1)',
-            data: [150, 73, 20],
-            pointPadding: 0.3,
-            pointPlacement: -0.2
-        }, {
-            name: 'Employees Optimized',
-            color: 'rgba(126,86,134,.9)',
-            data: [140, 90, 40],
-            pointPadding: 0.4,
-            pointPlacement: -0.2
-        }, {
-            name: 'Profit',
-            color: 'rgba(248,161,63,1)',
-            data: [183.6, 178.8, 198.5],
-            tooltip: {
-                valuePrefix: '$',
-                valueSuffix: ' M'
-            },
-            pointPadding: 0.3,
-            pointPlacement: 0.2,
-            yAxis: 1
-        }, {
-            name: 'Profit Optimized',
-            color: 'rgba(186,60,61,.9)',
-            data: [203.6, 198.8, 208.5],
-            tooltip: {
-                valuePrefix: '$',
-                valueSuffix: ' M'
-            },
-            pointPadding: 0.4,
-            pointPlacement: 0.2,
-            yAxis: 1
+            name: 'Volunteer Frequency',
+            colorByPoint: true,
+            data: [{
+                name: 'AB-7L',
+                y: 27.33,
+                sliced: true,
+            }, {
+                name: 'AB-2L',
+                y: 24.27,
+            }, {
+                name: 'AB-4L',
+                y: 15.88
+            }, {
+                name: 'AB-3L',
+                y: 13.79
+            }, {
+                name: 'AB-1L',
+                y: 8.92
+            }, {
+                name: 'AB-5L',
+                y: 5.21
+            }, {
+                name: 'AB-6L',
+                y: 4.6
+            }]
         }]
     });
+
+//     $('#cmsc128 #lec-class-student').highcharts({
+//         chart: {
+//             type: 'column'
+//         },
+//         title: {
+//             text: 'CMSC 128 Volunteer Count per Lab Class'
+//         },
+//         subtitle: {
+//             text: 'for the whole semester'
+//         },
+//         xAxis: {
+//             categories: [
+//                 'AB-1L',
+//                 'AB-2L',
+//                 'AB-3L',
+//                 'AB-4L',
+//                 'AB-5L',
+//                 'AB-6L',
+//                 'AB-7L',
+//             ],
+//             crosshair: true
+//         },
+//         yAxis: {
+//             min: 0,
+//             title: {
+//                 text: 'Volunteers'
+//             }
+//         },
+//         tooltip: {
+//             headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+//             pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+//                 '<td style="padding:0"><b>{point.y:.1f} students</b></td></tr>',
+//             footerFormat: '</table>',
+//             shared: true,
+//             useHTML: true
+//         },
+//         plotOptions: {
+//             column: {
+//                 pointPadding: 0.2,
+//                 borderWidth: 0
+//             }
+//         },
+//         series: [{
+//             name: 'Male',
+//             data: [5, 3, 2, 3, 2, 1, 4]
+
+//         }, {
+//             name: 'Female',
+//             data: [3, 2, 1, 5, 1, 4, 4]
+//         }]
+//     });
+// });
+
+// $('document').ready(function(){
+//     $('#cmsc132 #lec-class-lab').highcharts({
+//         chart: {
+//             plotBackgroundColor: null,
+//             plotBorderWidth: null,
+//             plotShadow: false,
+//             type: 'pie'
+//         },
+//         title: {
+//             text: 'CMSC 132 T Volunteer Distribution Per Lab Class'
+//         },
+//         tooltip: {
+//             pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+//         },
+//         plotOptions: {
+//             pie: {
+//                 allowPointSelect: true,
+//                 cursor: 'pointer',
+//                 dataLabels: {
+//                     enabled: true,
+//                     format: '<b>{point.name}</b>: {point.percentage:.1f} %</a>',
+//                     style: {
+//                         color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+//                     }
+//                 }
+//             }
+//         },
+//         series: [{
+//             name: 'Volunteer Frequency',
+//             colorByPoint: true,
+//             data: [{
+//                 name: 'T-6L',
+//                 y: 27.33,
+//             }, {
+//                 name: 'T-1L',
+//                 y: 24.27,
+//                 sliced: true,
+//             }, {
+//                 name: 'T-3L',
+//                 y: 15.88
+//             }, {
+//                 name: 'T-4L',
+//                 y: 13.79
+//             }, {
+//                 name: 'T-2L',
+//                 y: 9.91
+//             }, {
+//                 name: 'T-5L',
+//                 y: 8.98
+//             }]
+//         }]
+//     });
+
+    // $('#cmsc132 #lec-class-student').highcharts({
+    //     chart: {
+    //         type: 'column'
+    //     },
+    //     title: {
+    //         text: 'CMSC 132 Volunteer Count per Lab Class'
+    //     },
+    //     subtitle: {
+    //         text: 'for the whole semester'
+    //     },
+    //     xAxis: {
+    //         categories: [
+    //             'AB-1L',
+    //             'AB-2L',
+    //             'AB-3L',
+    //             'AB-4L',
+    //             'AB-5L',
+    //             'AB-6L',
+    //         ],
+    //         crosshair: true
+    //     },
+    //     yAxis: {
+    //         min: 0,
+    //         title: {
+    //             text: 'Volunteers'
+    //         }
+    //     },
+    //     tooltip: {
+    //         headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+    //         pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+    //             '<td style="padding:0"><b>{point.y:.1f} students</b></td></tr>',
+    //         footerFormat: '</table>',
+    //         shared: true,
+    //         useHTML: true
+    //     },
+    //     plotOptions: {
+    //         column: {
+    //             pointPadding: 0.2,
+    //             borderWidth: 0
+    //         }
+    //     },
+    //     series: [{
+    //         name: 'Male',
+    //         data: [5, 3, 2, 3, 2, 1]
+
+    //     }, {
+    //         name: 'Female',
+    //         data: [3, 2, 1, 5, 1, 4]
+    //     }]
+    // });
   });
